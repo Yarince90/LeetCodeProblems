e5512ce6ababcbf739304a96e394d4795d4168e6
Yarince90 <Yarincevigil@gmail.com>
2023-04-17 17:11:05 -0400

    Leet code problem 2 solved

--- Easy.cpp
+++ Easy.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 #include "Easy.h"
 
 using namespace std;
@@ -16,48 +17,23 @@ namespace leetCode {
 	//Problem 2
 	vector<int> twoSum(vector<int>& nums, int target) {
 
-		vector<int> res;
+        unordered_map<int, int> mapList;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            
+            int n = nums[i];            
+            int equalsTo = target - n;
+           
+            if (mapList.count(equalsTo)) {
 
-		if (nums.size() > 2) {
-			for (vector<int>::iterator it1 = nums.begin(); it1 != nums.end(); it1++) {
-				for (vector<int>::iterator it2 = nums.begin(); it2 != nums.end(); it2++) {
+                cout << mapList[equalsTo] << " " << i << endl;
 
-					cout << "Comparing nums: " << *it1 << " To reverse nums: " << *it2 << endl;
-
-					if (*it1 + *it2 == target) {
-						int el1, el2;
-						int key1 = *it1;
-						int key2 = *it2;
-
-						//Find Element based on it1/it2 values
-						it1 = find(nums.begin(), nums.end(), key1);
-						it2 = find(nums.begin(), nums.end(), key2);
-
-						el1 = distance(nums.begin(), it1);
-						el2 = distance(nums.begin(), it2);
-
-						//Check to see if the element is the same
-						if (el1 == el2) {
-
-						}
-
-						cout << el1 << " " << el2 << endl;
-
-						res = { el1,el2 };
-
-						return res;
-
-					}
-				}
-			}
-		}
-		else {
-			if (nums[0] + nums[1] == target) {
-				cout << 0 << " " << 1 << endl;
-				res = { 0,1 };
-				return res;
-			}
-		}
-		return res;
+                return { mapList[equalsTo], i };
+            }
+            
+            
+            mapList[n] = i;  // assume that each input would have exactly one solution
+        }
+        return {};
 	}
 }
